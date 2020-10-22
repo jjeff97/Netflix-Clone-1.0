5ce6efadba3ecf51b9958f7452f8e030e77508b6
James Jeffeson <jrjefferson830@gmail.com>
2020-10-21 22:11:19 -0500

    Banner build out

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Row from './Row';
 import requests from './requests';
+import Banner from "./Banner";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Let's build</h1>
+    <div className="app">
+      {/* Nav */}
+      <Banner/>
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
