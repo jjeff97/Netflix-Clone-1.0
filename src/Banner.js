2211c75f3b48c1743df951f02d280185228e5aa0
James Jeffeson <jrjefferson830@gmail.com>
2020-10-22 22:11:50 -0500

    Basic banner content dom render

--- src/Banner.js
+++ src/Banner.js
@@ -8,7 +8,7 @@ function Banner() {
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(requests.fetchNetflixOriginals);
-      console.log(
+      setMovie(
         request.data.results[
           Math.floor(Math.random() * request.data.results.length - 1)
         ]
@@ -20,27 +20,26 @@ function Banner() {
 
   console.log(movie);
   return (
-    <header className="banner"
-    style={{
-        backgroundSize: "cover",
+    <header
+      className="banner"
+      style={{
+        backgroundSize: 'cover',
         backgroundImage: `url(
           "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
         )`,
-        backgroundPosition: "center center",
-    }}
+        backgroundPosition: 'center center',
+      }}
     >
-     
-      <div classname="banner_contents">
-      {''}
-      {/* <<< Background Image */}
-      {/*title*/}
-      <h1>
-{movie?.title || movie?.name || movie?.original_name}
-      </h1>
-      {/*div >2 buttons */}
-      {/*description */}
+      <div className="banner_contents">
+        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        <div className="banner_buttons">
+          <button className="banner_button">Play</button>
+          <button className="banner_button">My List</button>
+        </div>
+        <h1 className="banner_description">
+          {movie?.overview}
+        </h1>
       </div>
-      
     </header>
   );
 }
