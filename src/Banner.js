2c9a9a8677149417c76d4f526cea84e43aba5b7e
James Jeffeson <jrjefferson830@gmail.com>
2020-10-25 14:17:30 -0500

    banner bottom fade button styling

--- src/Banner.js
+++ src/Banner.js
@@ -20,6 +20,11 @@ function Banner() {
   }, []);
 
   console.log(movie);
+
+  function truncate(str, n){
+return str?.length > n ? str.substr(0, n -1) + "..." : str;
+
+  }
   return (
     <header
       className="banner"
@@ -32,15 +37,16 @@ function Banner() {
       }}
     >
       <div className="banner_contents">
-        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        <h1 className="banner_title">{movie?.title || movie?.name || movie?.original_name}</h1>
         <div className="banner_buttons">
           <button className="banner_button">Play</button>
           <button className="banner_button">My List</button>
         </div>
         <h1 className="banner_description">
-          {movie?.overview}
+          {truncate(movie?.overview, 200)}
         </h1>
       </div>
+      <div className="banner--fadeBottom"/>
     </header>
   );
 }
